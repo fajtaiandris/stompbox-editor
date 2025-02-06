1228650b55c3d0e4d28761cf0dfc986a58cc029e
andris <andris.fajkusz@gmail.com>
2025-02-06 16:19:34 +0100

    fix: styling

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -28,7 +28,7 @@ export const Menu = () => {
     return <RowMenu />
   }
   return (
-    <div className="absolute top-[20px] left-[20px] h-[500px] rounded-[6px] border-2 border-[lightgray] shadow-lg">
+    <div className="absolute top-[20px] left-[20px] h-[500px] rounded-[6px] border shadow-lg">
       <div className="h-full w-[200px] overflow-scroll rounded-[6px] bg-white p-[10px]">
         <div className="text-gray-400">{getLabel()}</div>
       </div>
